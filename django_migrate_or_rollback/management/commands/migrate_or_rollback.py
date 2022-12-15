b768135a0cb2ae2c31530ddf71d87947f2d01087
Benjamin Zagorsky <zags@users.noreply.github.com>
2022-12-15 15:22:02 -0500

    Implements migrate_or_rollback management command, version 0.0.1

---
+++ django_migrate_or_rollback/management/commands/migrate_or_rollback.py
@@ -0,0 +1,35 @@
+from django.db.migrations.recorder import MigrationRecorder
+from django.db.migrations.loader import MigrationLoader
+from django.core.management import call_command
+from django.conf import settings
+from django.core.management.commands import migrate
+from django.db import connections
+
+from pprint import pprint
+
+
+class Command(migrate.Command):
+    def handle(self, *args, **options):
+        db = options.get('database')
+        connection = connections[db]
+        connection.prepare_database()
+        loader = MigrationLoader(connection, ignore_no_migrations=True)
+        loader.applied_migrations
+        
+        extant_migrations = {k[0]: v for k, v in loader.applied_migrations.items() if k in loader.graph.nodes}
+        last_migrations = {}
+        for app, migration in extant_migrations.items():
+            if app not in last_migrations or migration.applied > last_migrations[app].applied:
+                last_migrations[app] = migration
+        last_migration_names = {app: migration.name for app, migration in last_migrations.items()}
+        
+        try:
+            super().handle(*args, **options)
+        except Exception:
+            print("\n\nMigrating failed; rolling back to last migration state:\n")
+            pprint(last_migration_names)
+            print()
+            for app, migration_name in last_migration_names.items():
+                call_command("migrate", app, migration_name)
+            print("\nRollboack successfull\n")
+            raise
---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+import setuptools
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+setuptools.setup(
+    author="Zagaran, Inc.",
+    author_email="info@zagaran.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    description="Management command to attempt Django migrations and rollback on failure",
+    keywords="django migrations",
+    license="MIT",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    name="migrate-or-rollback",
+    packages=setuptools.find_packages(),
+    install_requires=["django >= 2.0"],
+    python_requires='>=3.6',
+    url="https://github.com/zagaran/django-migrate-or-rollback",
+    version="0.0.1",
+)
